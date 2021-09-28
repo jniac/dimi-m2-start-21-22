f15e633a0383595d3a26e60ffb6f5af2e52e396e
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2021-09-28 16:57:23 +0200

    complete! (style + innerHTML)

--- guess-the-number-console/main.js
+++ guess-the-number-console/main.js
@@ -2,16 +2,26 @@ console.log('Hello user!')
 console.log('Try to guess the mystery number.')
 console.log('Use "submit(x)" to submit a number.')
 
-const mysteryNumber = 45
+const mysteryNumber = Math.round(Math.random() * 100)
 
 const submit = (x) => {
   if (x < mysteryNumber) {
     console.log('Too small.')
+    document.body.innerHTML = '<h1>Too small.</h1>'
+    document.body.style.backgroundColor = '#8e7cdb'
   }
   if (x > mysteryNumber) {
     console.log('Too big.')
+    document.body.innerHTML = '<h1>Too big.</h1>'
+    document.body.style.backgroundColor = '#db7cb5'
   }
   if (x == mysteryNumber) {
     console.log('Hurrah!')
+    document.body.innerHTML = '<h1>Hurrah!</h1>'
+    document.body.style.backgroundColor = '#97ffe7'
   }
+}
+
+const cheat = () => {
+  console.log(`The mystery number is ${mysteryNumber}`)
 }
