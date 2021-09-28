234d099d68fbb13c1a835e2fd051700d71bc5a0b
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2021-09-28 15:39:29 +0200

    almost done

--- guess-the-number-console/main.js
+++ guess-the-number-console/main.js
@@ -3,3 +3,15 @@ console.log('Try to guess the mystery number.')
 console.log('Use "submit(x)" to submit a number.')
 
 const mysteryNumber = 45
+
+const submit = (x) => {
+  if (x < mysteryNumber) {
+    console.log('Too small.')
+  }
+  if (x > mysteryNumber) {
+    console.log('Too big.')
+  }
+  if (x == mysteryNumber) {
+    console.log('Hurrah!')
+  }
+}
