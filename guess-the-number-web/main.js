62048c67c2396d69da035448641bbb3b301262a5
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2021-10-06 13:02:17 +0200

    introducing javascript

---
+++ guess-the-number-web/main.js
@@ -0,0 +1,37 @@
+
+// INIT
+
+const mysteryNumber = Math.round(Math.random() * 100)
+
+// je récupère, mémorise et supprime le nœud div.response
+const responseElement = document.querySelector('div.response')
+responseElement.remove()
+
+const inputElement = document.querySelector('input')
+
+
+
+// RUNTIME
+
+inputElement.onchange = (event) => {
+  
+  let x = inputElement.value
+
+  x = parseFloat(x)
+
+  if (Number.isNaN(x)) {
+
+    const clone = responseElement.cloneNode(true)
+    document.body.append(clone)
+    clone.querySelector('span.input').innerHTML = x
+    clone.querySelector('span.comment').innerHTML = 'Wrong input!!!'
+
+  } else if (x < 0 || x > 100) {
+
+  } else if (x < mysteryNumber) {
+
+  } else if (x > mysteryNumber) {
+
+  } else if (x == mysteryNumber) {
+  }
+}
