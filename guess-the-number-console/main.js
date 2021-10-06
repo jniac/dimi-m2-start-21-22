94618b6dfd2ebac83f73ffb6fc1d06b76aa12ea8
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2021-10-06 11:21:50 +0200

    raw animation

--- guess-the-number-console/main.js
+++ guess-the-number-console/main.js
@@ -10,27 +10,27 @@ const submit = (x) => {
 
   if (Number.isNaN(x)) {
     console.log('Wrong input, please give a number.')
-    document.body.innerHTML = '<h1>⚠️</h1>'
+    document.body.innerHTML = '<h1 class="bounce">⚠️</h1>'
     document.body.style.backgroundColor = 'red'
 
   } else if (x < 0 || x > 100) {
     console.log('Number should be between 0 & 100.')
-    document.body.innerHTML = '<h1>Number should be between 0 & 100.</h1>'
+    document.body.innerHTML = '<h1 class="bounce">Number should be between 0 & 100.</h1>'
     document.body.style.backgroundColor = 'red'
 
   } else if (x < mysteryNumber) {
     console.log('Too small.')
-    document.body.innerHTML = '<h1>Too small.</h1>'
+    document.body.innerHTML = '<h1 class="bounce">Too small.</h1>'
     document.body.style.backgroundColor = '#8e7cdb'
 
   } else if (x > mysteryNumber) {
     console.log('Too big.')
-    document.body.innerHTML = '<h1>Too big.</h1>'
+    document.body.innerHTML = '<h1 class="bounce">Too big.</h1>'
     document.body.style.backgroundColor = '#db7cb5'
 
   } else if (x == mysteryNumber) {
     console.log('Hurrah!')
-    document.body.innerHTML = '<h1>Hurrah!</h1>'
+    document.body.innerHTML = '<h1 class="bounce">Hurrah!</h1>'
     document.body.style.backgroundColor = '#97ffe7'
   }
 }
