27901de1b4def3d5956f04bf5eebc6b0eb6ade1e
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2021-10-06 11:14:44 +0200

    check input (parseFloat, isNaN etc.)

--- guess-the-number-console/main.js
+++ guess-the-number-console/main.js
@@ -5,17 +5,30 @@ console.log('Use "submit(x)" to submit a number.')
 const mysteryNumber = Math.round(Math.random() * 100)
 
 const submit = (x) => {
-  if (x < mysteryNumber) {
+
+  x = parseFloat(x)
+
+  if (Number.isNaN(x)) {
+    console.log('Wrong input, please give a number.')
+    document.body.innerHTML = '<h1>⚠️</h1>'
+    document.body.style.backgroundColor = 'red'
+
+  } else if (x < 0 || x > 100) {
+    console.log('Number should be between 0 & 100.')
+    document.body.innerHTML = '<h1>Number should be between 0 & 100.</h1>'
+    document.body.style.backgroundColor = 'red'
+
+  } else if (x < mysteryNumber) {
     console.log('Too small.')
     document.body.innerHTML = '<h1>Too small.</h1>'
     document.body.style.backgroundColor = '#8e7cdb'
-  }
-  if (x > mysteryNumber) {
+
+  } else if (x > mysteryNumber) {
     console.log('Too big.')
     document.body.innerHTML = '<h1>Too big.</h1>'
     document.body.style.backgroundColor = '#db7cb5'
-  }
-  if (x == mysteryNumber) {
+
+  } else if (x == mysteryNumber) {
     console.log('Hurrah!')
     document.body.innerHTML = '<h1>Hurrah!</h1>'
     document.body.style.backgroundColor = '#97ffe7'
@@ -24,4 +37,8 @@ const submit = (x) => {
 
 const cheat = () => {
   console.log(`The mystery number is ${mysteryNumber}`)
+}
+
+const distance = (x, y) => {
+  return 
 }
